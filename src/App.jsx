6028b56ccae1b4e727d281f9273ce3c013d6b968
Flaminia Mazzitelli <flaminiamazzitelli@Flaminias-MBP.fritz.box>
2025-02-10 15:24:22 +0100

    added commits from another repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Title from './components/Title.jsx'
+import './App.scss'
+import Button from './components/atoms/Button/Button.jsx'
+import NavigationMenu from './components/molecules/NavigationMenu/NavigationMenu.jsx'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log("FROM APP!!!!");
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite ciao</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is number! {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        This is my website
-      </p>
+     {/* <Title text="helloooooo"/> */}
+
+      <NavigationMenu />
+
+{/* 
+    
+      <Button type='primary' text='Scopri come posso aiutarti'/>
+      <Button type='secondary' text='Scopri di piu'/>
+      <Button type='tertiary' text='Prendi appuntamento'/> */}
+
+
     </>
   )
 }
