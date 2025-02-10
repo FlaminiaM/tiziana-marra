1e96d9c525b677252a4ae4a9c21d56fd36dad7ef
Flaminia Mazzitelli <flaminiamazzitelli@Flaminias-MBP.fritz.box>
2025-02-10 16:52:06 +0100

    created header

--- src/components/atoms/Logo/Logo.jsx
+++ src/components/atoms/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import './Logo.scss';
-import logo from '../../../assets/logo-3.png';
-import LogoSvg from '../../../assets/logo-4.png?react';
+import logo from '../../../assets/logo-7.png';
+import LogoSvg from '../../../assets/logo-5.png?react';
 
 
 function Logo({}) {
     return (
         <div className='logo'>
-            {/* <img src={logo} alt='logo' /> */}
+            <img src={logo} alt='logos' />
             {/* <LogoSvg /> */}
        </div>
     )
