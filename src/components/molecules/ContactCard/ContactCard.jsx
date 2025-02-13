a488bb114c481f46c4b279acef00549324e167cc
Flaminia Mazzitelli <flaminiamazzitelli@Flaminias-MBP.fritz.box>
2025-02-13 10:29:12 +0100

    created contact card

--- src/components/molecules/ContactCard/ContactCard.jsx
+++ src/components/molecules/ContactCard/ContactCard.jsx
@@ -1,15 +1,13 @@
 import './ContactCard.scss';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import HomeIcon from '../../../assets/home-icon.svg?react'
 
 function ContactCard({icon, title, text}) {
     return (
         <div className='contact-card'>
-            <div className='contact-card-icon'>{icon}</div>
-            <div className='contact-card-title'>{title}</div>
+            <div className='contact-card-icon'><HomeIcon /></div>
+            <h4 className='contact-card-title'>{title}</h4>
             <div className='contact-card-text'>{text}</div>
-            <FontAwesomeIcon icon={faCoffee} />
         </div>
     )
 
